cd39fabf8418ffc3836052937bdf9bb4fbf68e97
Domanic Gill <gilldomanic@gmail.com>
2018-10-07 20:25:03 +0100

    Created action button menu. Visually complete Functionality of nav bar and action button needs working

--- app/src/main/java/dom/notescanner/MainActivity.java
+++ app/src/main/java/dom/notescanner/MainActivity.java
@@ -6,14 +6,26 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawer;
+    FloatingActionMenu floatingActionMenu;
+    FloatingActionButton fabAddPhoto, fabAddGallery, fabaddNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionMenu = findViewById(R.id.floatingActionMenu);
+        fabAddPhoto = findViewById(R.id.fabItem1);
+        fabAddGallery = findViewById(R.id.fabItem2);
+        fabaddNote = findViewById(R.id.fabItem3);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -33,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    public void onClick (View v) {
+        if(v.getId() == R.id.fabItem1 || v.getId() == R.id.nav_photo) {
+            Toast.makeText(MainActivity.this,"photo click",Toast.LENGTH_SHORT).show();
+        } else {
+            floatingActionMenu.close(true);
         }
     }
 }
